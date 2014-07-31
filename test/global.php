c93a5e41f6b20553081db4a282f27b2a1f502a7b
Emmanuel Bouton <goten4@gmail.com>
2014-07-31 12:35:00 +0200

    Add route signout for tests

--- test/global.php
+++ test/global.php
@@ -7,6 +7,16 @@ return [
     ],
     'router' => [
         'routes' => [
+            'signout' => [
+                'type' => 'Literal',
+                'options' => [
+                    'route' => '/signout',
+                    'defaults' => [
+                        'controller' => 'KmbOabAuthentication\Controller\Signout',
+                        'action' => 'index',
+                    ],
+                ],
+            ],
             'index' => [
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => [
